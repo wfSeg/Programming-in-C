1701152f9e2e05ce91d57388aff2a9c4a3a5cc1d
wfSeg <ninjacenterfold@gmail.com>
2015-05-19 16:36:43 -0700

    Create ex03-05.c

---
+++ Exercise/Chapter-3/ex03-05.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int sum;		// capitalization
+	/*compute result */	//need to close with */
+	sum = 25 + 37 -19;		// need the ;
+	/* display reults */ 	// use correct closing slashes, probably need printf what question was in first place
+	printf ("The sum of 25 + 37 - 19 is %i\n", sum);
+	return 0;
+}
