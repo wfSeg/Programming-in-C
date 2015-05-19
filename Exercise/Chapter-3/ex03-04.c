242115e2ce8e11709c13c2313de2f75a0e98df70
wfSeg <ninjacenterfold@gmail.com>
2015-05-19 16:32:16 -0700

    Create ex03-04.c
    Can't believe this took me 3 tries -_- (doing this in afternoon, after lunch)
    
    longn@us-longn ~/C
    $ ./a.exe
    Subtracting 87 from 15 gives 72
    
    longn@us-longn ~/C
    $ gcc ex03-04.c
    
    longn@us-longn ~/C
    $ ./a.exe
    Subtracting 87 from 15 gives -72
    
    longn@us-longn ~/C
    $ gcc ex03-04.c
    
    longn@us-longn ~/C
    $ ./a.exe
    Subtracting 15 from 87 gives 72

---
+++ Exercise/Chapter-3/ex03-04.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main (void)
+{
+	int value1, value2, sum;
+	value1 = 15;
+	value2 = 87;
+	sum = value2 - value1;
+	
+	printf ("Subtracting %i from %i gives %i\n", value1, value2, sum);
+	return 0;
+}
